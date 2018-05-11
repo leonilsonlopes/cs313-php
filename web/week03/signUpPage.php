e1e5bf75d87295b387a9af9ad2cf1b3d75af7ff4
Leonilson <leonilson.lopes1@gmail.com>
2018-05-12 00:32:52 +0100

    week03 ponder

--- web/week03/signUpPage.php
+++ web/week03/signUpPage.php
@@ -69,8 +69,26 @@
 				</div>
 				-->
 				
+				  <!-- Modal -->
+				<div class="modal fade" id="signUpSuccess" role="dialog">
+					<div class="modal-dialog modal-lg">
+					<div class="modal-content">
+						<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4 class="modal-title"><p>Shopping Cart Updated!</p></h4>
+						</div>
+						<div class="modal-body">
+						<p id="itemAddedToCart"></p>
+						</div>
+						<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						</div>
+					</div>
+					</div>
+				</div>
+								
 				<div class="form-group">					
-					<input type="button" class="btn btn-info" value="Save Data" onclick="signUpSaveData()">
+					<input type="button" class="btn btn-info" value="Save Data" onclick="signUpSaveData()" data-toggle="modal" data-target="#signUpSuccess>
 				</div>
 				<div class="messagePanel" id="signUpSuccessMessage">
 					<div class="alert alert-success">

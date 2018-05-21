47a432ee7b9641dd8f42159b1be2902c40e81b14
Leonilson <leonilson.lopes1@gmail.com>
2018-05-21 23:49:45 +0100

    week05 prepare

---
+++ web/week05_prepare/index.php
@@ -0,0 +1,16 @@
+<?php
+
+try
+{
+  $user = 'hnemadwwvpxdyg';
+  $password = 'c7ed2bccf9d743009848bd7533759aad0749a69754c167e58aa8c00891ff3bfe';
+  $db = new PDO('pgsql:host=ec2-54-243-137-182.compute-1.amazonaws.com;dbname=d1kgd7up2ibtgf', $user, $password);
+  echo '<p>SUCCESS</p>';
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?>

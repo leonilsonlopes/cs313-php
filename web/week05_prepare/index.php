42653a97b8493134e950186753b7157078b456c0
Leonilson <leonilson.lopes1@gmail.com>
2018-05-21 23:51:13 +0100

    week05 prepare

--- web/week05_prepare/index.php
+++ web/week05_prepare/index.php
@@ -3,7 +3,7 @@
 try
 {
   $user = 'hnemadwwvpxdyg';
-  $password = 'c7ed2bccf9d743009848bd7533759aad0749a69754c167e58aa8c00891ff3bfe';
+  $password = 'xc7ed2bccf9d743009848bd7533759aad0749a69754c167e58aa8c00891ff3bfe';
   $db = new PDO('pgsql:host=ec2-54-243-137-182.compute-1.amazonaws.com;dbname=d1kgd7up2ibtgf', $user, $password);
   echo '<p>SUCCESS</p>';
 }

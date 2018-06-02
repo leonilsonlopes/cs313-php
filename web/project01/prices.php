b8e6ed1820dcb0b0804ae16346e5b8ba607bbc4a
Leonilson <leonilson.lopes1@gmail.com>
2018-06-03 00:39:56 +0100

    week06 project01

--- web/project01/prices.php
+++ web/project01/prices.php
@@ -24,9 +24,9 @@
 	
 	<?php 
 		$api = new Binance\API();
-		$ticker = $api->prices();
-		print_r($ticker); // List prices of all symbols
-		echo "Price of BNB: {$ticker['BNBBTC']} BTC.".PHP_EOL;
+		//$ticker = $api->prices();
+		//print_r($ticker); // List prices of all symbols
+		//echo "Price of BNB: {$ticker['BNBBTC']} BTC.".PHP_EOL;
 	?>
 	
 	<!-- CLOSE CONTENT AREA -->	

fb7b4c927a2f9b1d732e26418754ec21d65a7cd0
Leonilson <leonilson.lopes1@gmail.com>
2018-06-02 10:07:59 +0100

    week06 project01

--- web/project01/dbConnect.php
+++ web/project01/dbConnect.php
@@ -5,7 +5,7 @@ function get_db() {
 	try {
 		// default Heroku Postgres configuration URL
 		$dbUrl = getenv('DATABASE_URL_PROJECT01');
-		echo $dbUrl;
+		
 		// Get the various parts of the DB Connection from the URL
 		$dbopts = parse_url($dbUrl);
 		$dbHost = $dbopts["host"];
@@ -17,6 +17,14 @@ function get_db() {
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
 		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		
+		echo "<p><h3>dboopts: " . $dbUrl . "</h3></p>"; 
+		echo "<p><h3>dboopts: " . $dbopts . "</h3></p>"; 
+		echo "<p><h3>dboopts: " . $dbHost . "</h3></p>"; 
+		echo "<p><h3>dboopts: " . $dbPort . "</h3></p>"; 
+		echo "<p><h3>dboopts: " . $dbUser . "</h3></p>"; 
+		echo "<p><h3>dboopts: " . $dbPassword . "</h3></p>"; 
+		echo "<p><h3>dboopts: " . $dbName . "</h3></p>"; 
 	}
 	catch (PDOException $ex) {
 		// If this were in production, you would not want to echo

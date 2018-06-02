3e7cbad60e35a5997b9649c5ef0f633e190c06b2
Leonilson <leonilson.lopes1@gmail.com>
2018-06-02 10:29:46 +0100

    week06 project01

--- web/project01/cryptoCurrencies.php
+++ web/project01/cryptoCurrencies.php
@@ -16,7 +16,51 @@
 		</div>
 	
 
-	<?php include 'databaseConnections.php' ?>;
+		<div class="container">
+			<h2>Crypto Currencies</h2>
+			<div class="panel panel-default">
+				<div class="panel-body">Insert the crypto coin you want to follow here</div>
+			</div>
+		</div>
+	
+		<div class=\"form-group\">
+			<label for=\"code\">Coin Code:</label>
+			<input type=\"text\" class=\"form-control\" id=\"code\">
+		</div>
+		<div class=\"form-group\">
+			<label for=\"name\">Coin Name:</label>
+			<input type=\"text\" class=\"form-control\" id=\"name\">
+		</div>
+		<button type=\"button\" class=\"btn btn-primary\">Save Coin</button>
+		<br/>
+		<br/>
+		<p><h3><b>Saved Coins:</b></h3></p>
+		<table class=\"table\">
+			<thead>
+				<tr>
+				<th scope=\"col\">#</th>
+				<th scope=\"col\">Coin Code</th>
+				<th scope=\"col\">Coin Name</th>
+				</tr>
+			</thead>
+			<tbody>
+
+		<!-- Retrieve Data -->	
+		<?php 
+			$db = get_db();
+			foreach ($db->query('SELECT * FROM currency') as $row){
+				echo '<tr>
+				<th scope=\"row\">' . $row['id'] . '</th>
+				<td>' . $row['code'] . '</td>
+				<td>' . $row['name'] . '</td>
+				</tr>';
+			} 
+		?>;
+			</tbody>
+		</table>
+		
+	
+	
 	<!-- CLOSE CONTENT AREA -->	
 	
 	</div>	

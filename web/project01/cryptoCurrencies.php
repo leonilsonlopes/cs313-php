c0117cfdde5106ced915b0cc3a6e2caade3134dc
Leonilson <leonilson.lopes1@gmail.com>
2018-06-02 11:53:01 +0100

    week06 project01

--- web/project01/cryptoCurrencies.php
+++ web/project01/cryptoCurrencies.php
@@ -61,7 +61,7 @@
 		<br/>
 		<br/>
 		<p><h3><b>Saved Coins:</b></h3></p>
-		<table id="currencies" class="table table-striped table-bordered" style="width:100%">
+		<table id="currencies" class="table table-hover table-bordered" style="width:100%">
 			<thead>
 				<tr>
 				<th scope="col">#</th>

a7255263ec6b95a206d35f334432395d5ce43733
Leonilson <leonilson.lopes1@gmail.com>
2018-06-02 09:58:47 +0100

    week06 project01

--- web/project01/cryptoCurrencies.php
+++ web/project01/cryptoCurrencies.php
@@ -1,3 +1,4 @@
+<?php include 'dbConnect.php';?>
 <!DOCTYPE html>
 <html lang="en">
 	
@@ -7,6 +8,10 @@
 	<?php include 'breadCrumbs.php';?>
 
 	<!-- START CONTENT AREA -->
+	
+	
+	
+	
 	  
 		<div class="container">
 			<h2>Crypto Currencies</h2>
@@ -15,8 +20,44 @@
 			</div>
 		</div>
 	
+		<div class=\"form-group\">
+			<label for=\"code\">Coin Code:</label>
+			<input type=\"text\" class=\"form-control\" id=\"code\">
+		</div>
+		<div class=\"form-group\">
+			<label for=\"name\">Coin Name:</label>
+			<input type=\"text\" class=\"form-control\" id=\"name\">
+		</div>
+		<button type=\"button\" class=\"btn btn-primary\">Save Coin</button>
+		<br/>
+		<br/>
+		<p><h3><b>Saved Coins:</b></h3></p>
+		<table class=\"table\">
+			<thead>
+				<tr>
+				<th scope=\"col\">#</th>
+				<th scope=\"col\">Coin Code</th>
+				<th scope=\"col\">Coin Name</th>
+				</tr>
+			</thead>
+			<tbody>
 
-	<?php include 'databaseConnections.php' ?>;
+		<!-- Retrieve Data -->	
+		<?php foreach ($db->query('SELECT * FROM currency') as $row){
+				echo '<tr>
+				<th scope=\"row\">' . $row['id'] . '</th>
+				<td>' . $row['code'] . '</td>
+				<td>' . $row['name'] . '</td>
+				</tr>';
+			} 
+		?>;
+			</tbody>
+		</table>
+		
+	
+	
+	
+	
 	<!-- CLOSE CONTENT AREA -->	
 	
 	</div>	

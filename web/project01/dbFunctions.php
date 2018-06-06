2b8fb98514420ee5c3cea7428c1d4aa74afbcf08
Leonilson <leonilson.lopes1@gmail.com>
2018-06-06 07:36:37 +0100

    week06 project01

--- web/project01/dbFunctions.php
+++ web/project01/dbFunctions.php
@@ -1,7 +1,7 @@
 <?php include 'dbConnect.php';?>
 <?php
-function getListOfCurrencies(){ 
-	$db = get_db();
+$db = get_db();
+function getListOfCurrencies(){ 	
 	$db->query('SELECT * FROM currency');
 	return $db
 }

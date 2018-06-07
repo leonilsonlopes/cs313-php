0fd456be083bd15ba262280e18bd21cbf4554667
Leonilson <leonilson.lopes1@gmail.com>
2018-06-08 00:45:06 +0100

    week06 project01

--- web/project01/trading.php
+++ web/project01/trading.php
@@ -37,12 +37,11 @@
 				
 				</div>
 			</div>	
-				<input type="text" id="selectedCoin" />
+				<input type="text" id="selectedCoin" value="fixed" />
 		</form>
 		
 		<?php
-			echo "<p><h1>" . var_dump($availableCoins) . "</h1></p>";
-			echo "<br/><br/><p><h1>BTC:::: " . $_POST['selectedCoin'] . "</h1></p>";
+			echo "<p><h1>SELECTED:::: " . $_POST['selectedCoin'] . "</h1></p>";
 
 		?>
 		

a29e940060229f4004d2d0e3cae2930827560c6d
Leonilson <leonilson.lopes1@gmail.com>
2018-06-08 00:16:05 +0100

    week06 project01

--- web/project01/trading.php
+++ web/project01/trading.php
@@ -37,12 +37,12 @@
 				
 				</div>
 			</div>	
-				<input type="hidden" name="selectedCoin" />
+				<input type="hidden" id="selectedCoin" />
 		</form>
 		
 		<?php
 			echo "<p><h1>" . var_dump($availableCoins) . "</h1></p>";
-			echo "<br/><br/><p><h1>BTC:::: " . selectedCoin . "</h1></p>";
+			echo "<br/><br/><p><h1>BTC:::: " . $_POST['selectedCoin'] . "</h1></p>";
 
 		?>
 		

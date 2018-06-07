21fd5338e0dffa4619c7872d5c7582abe105a7a5
Leonilson <leonilson.lopes1@gmail.com>
2018-06-07 07:18:57 +0100

    week06 project01

--- web/project01/header.php
+++ web/project01/header.php
@@ -9,8 +9,8 @@ echo "<head>
   <!-- Bootstrap core CSS-->
   <script type=\"text/javascript\" language=\"javascript\" src=\"https://code.jquery.com/jquery-3.3.1.js\"></script>
   <script type=\"text/javascript\" language=\"javascript\" src=\"https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js\"></script>
-  <link href=\"/docs/4.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">
-  <!-- Custom fonts for this template-->
+  <link href=\"vendor/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">
+   <!-- Custom fonts for this template-->
   <link href=\"vendor/font-awesome/css/font-awesome.min.css\" rel=\"stylesheet\" type=\"text/css\">
   <!-- Custom styles for this template-->
   <link href=\"css/sb-admin.css\" rel=\"stylesheet\">

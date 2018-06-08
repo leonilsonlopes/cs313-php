788016d2cc4416f874de63b9369726c266fb05da
Leonilson <leonilson.lopes1@gmail.com>
2018-06-08 23:53:59 +0100

    week06 project01

--- web/project01/trading.php
+++ web/project01/trading.php
@@ -78,7 +78,7 @@
 		?> 
 		
 		<br/>
-				<!--<form action="trading.php" method="post">
+				<!--<form action="trading.php" method="post">-->
 				<div class="input-group mb-3">
 					<div class="input-group-prepend">
 					<span class="input-group-text" id="inputGroup-sizing-default">Enter quantity:</span>
@@ -86,7 +86,7 @@
 					<input type="text" id="qtty" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
 					<button type="submit" class="btn btn-success" name="btnBuyCoin">Buy <b></b> Coin</button>
 				</div>				
-				</form>-->
+				<!--</form>-->
 			
 			<br/>
 			<p><h3><b>Open Buy Orders:</b></h3></p>

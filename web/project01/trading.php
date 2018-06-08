0c17cb747e5a2bd606aab08a13bd4a5b68e1669c
Leonilson <leonilson.lopes1@gmail.com>
2018-06-09 00:00:26 +0100

    week06 project01

--- web/project01/trading.php
+++ web/project01/trading.php
@@ -81,15 +81,15 @@
 		?> 
 		
 		<br/>
-				<!--<form action="trading.php" method="post">-->
+				
 				<div class="input-group mb-3">
 					<div class="input-group-prepend">
 					<span class="input-group-text" id="inputGroup-sizing-default">Enter quantity:</span>
 					</div>
 					<input type="text" id="qtty" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-					<button type="submit" class="btn btn-success" name="btnBuyCoin">Buy <b><?php echo $selectedCoin ?></b> Coin</button>
+					<button type="submit" class="btn btn-success" name="btnBuyCoin" value="buyCoin">Buy <b><?php echo $selectedCoin ?></b> Coin</button>
 				</div>				
-				<!--</form>-->
+				
 			
 			<br/>
 			<p><h3><b>Open Buy Orders:</b></h3></p>
@@ -108,7 +108,29 @@
 				</tbody>
 			</table>
 		
-
+			<?php 	
+			
+			if($_POST['btnBuyCoin'] == "buyCoin"){
+				echo "################################";
+				foreach (getListOfCurrencies() as $row){				
+					if(strtoupper($row['code']) == $selectedCoin ){
+						$coinData = getCoinInfo($row['code']);
+						echo '<tr>
+						<th scope=\"row\">' . $row['code'] . '</th>				
+						<td>' . $row['name'] . '</td>
+						<td>' . $coinData['price_usd'] . '</td>
+						<td>' . $coinData['percent_change_1h'] . '</td>
+						<td>' . $coinData['percent_change_24h'] . '</td>
+						<td>' . $coinData['percent_change_7d'] . '</td>
+						<td>' . $coinData['last_updated'] . '</td>
+						</tr>';
+						break;
+					}
+				}
+			}
+			echo "</tbody></table>";
+				
+		?>
 	
 
 	</form>

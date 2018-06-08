7fcc007a0d98c28317c15c1e1842240a7079ecbc
Leonilson <leonilson.lopes1@gmail.com>
2018-06-08 20:44:01 +0100

    week06 project01

--- web/project01/cryptoCurrencies.php
+++ web/project01/cryptoCurrencies.php
@@ -88,7 +88,7 @@ $(document).ready(function() {
 							echo"<br/><br/>
 								<div class=\"alert alert-danger alert-dismissible\">
 									<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>
-									<strong>" . $coinCode . "!</strong> coin symbol is not valid! Please enter a valid coin. To check valid coins, refer to https://coinmarketcap.com/.
+									<strong>" . $coinCode . "</strong> coin symbol is not valid! Please enter a valid coin. To check valid coins, refer to https://coinmarketcap.com/.
 								</div>";
 						}else{
 							$statement = $db->prepare('INSERT INTO currency(code, name) VALUES(:coinCode, :coinName)');

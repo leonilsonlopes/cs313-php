1a7812e6fcb106114e95fa20decfd57208f1ed52
Leonilson <leonilson.lopes1@gmail.com>
2018-06-08 21:26:21 +0100

    week06 project01

--- web/project01/phpAuxFunctions.php
+++ web/project01/phpAuxFunctions.php
@@ -1,6 +1,6 @@
 <?php
 function showAlert($message, $strong, $type){
-echo"<br/><div class=\"alert alert-" . . " alert-dismissible\">
+echo"<br/><div class=\"alert alert-" . $type . " alert-dismissible\">
 		<a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\" >&times;</a>
 		<strong>" . $strong . "</strong> " . $message . "
 	</div>";

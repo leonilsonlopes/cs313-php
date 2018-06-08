5a6b88c317726dc57f4c9450b5d7dea4df81d162
Leonilson <leonilson.lopes1@gmail.com>
2018-06-08 01:32:35 +0100

    week06 project01

--- web/project01/coinFunctions.php
+++ web/project01/coinFunctions.php
@@ -7,6 +7,10 @@ function getCoinInfo($coinCode){
 	
 	for ( $i=0; $i<$numCoinbaseCoins; $i++) {   
 		if(strtoupper($coinCode) == strtoupper($coinData[$i]['symbol'])){
+			$coinData[$i]['price_usd'] = '$' . $coinData['price_usd'];
+			$coinData[$i]['percent_change_1h'] = $coinData[$i]['percent_change_1h'] . '%';
+			$coinData[$i]['percent_change_24h'] = $coinData[$i]['percent_change_24h'] . '%';
+			$coinData[$i]['percent_change_7d'] = $coinData[$i]['percent_change_7d'] . '%';
 			$coinData[$i]['last_updated'] = date('m/d/Y h:i:s A', $coinData[$i]['last_updated']);
 			return $coinData[$i];
 		}else{

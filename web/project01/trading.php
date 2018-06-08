86ec5368b149731d661faef5e41c4195bca52bcb
Leonilson <leonilson.lopes1@gmail.com>
2018-06-08 23:03:32 +0100

    week06 project01

--- web/project01/trading.php
+++ web/project01/trading.php
@@ -71,14 +71,27 @@
 					</tr>';
 					break;
 				}
-			}			
+			}
+
+			echo "
+				<form action=\"trading.php\" method=\"post\">
+				<div class=\"input-group mb-3\">
+					<div class=\"input-group-prepend\">
+					<span class=\"input-group-text\" id=\"inputGroup-sizing-default\">Enter quantity:</span>
+					</div>
+					<input type=\"text\" id=\"qtty\" class=\"form-control\" aria-label=\"Default\" aria-describedby=\"inputGroup-sizing-default\">
+				</div>
+				<button type=\"submit\" class=\"btn btn-success\" name=\"btnBuyCoin\">Buy Coin</button>
+				</form>
+				"	;
 		?> 
 		
 			</tbody>
 		</table>
-	
+		<br/>
 		
-		<br/><br/>
+		
+
 
 </div>
 	

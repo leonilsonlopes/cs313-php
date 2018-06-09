f0553f98dc235aa01ac24c3d59685f66a5ce0a44
Leonilson <leonilson.lopes1@gmail.com>
2018-06-09 07:56:47 +0100

    week06 project01

--- web/project01/dbFunctions.php
+++ web/project01/dbFunctions.php
@@ -7,6 +7,7 @@ function getListOfCurrencies(){
 }
 
 function saveBuyOrder($coinCode, $price, $quantity, $total){
+	$db = get_db();
 	$statement = $db->prepare('INSERT INTO buy_order(currency_id, price, quantity, total) VALUES((select id from currency where code = :coinCode), :price, :quantity, :total)');
 	$statement->bindValue(':coinCode', $coinCode);
 	$statement->bindValue(':price', $price);

78a33abefdb6d393f8f7b911677b80fff3a203b2
Leonilson <leonilson.lopes1@gmail.com>
2018-06-09 23:53:43 +0100

    week06 project01

--- web/project01/cryptoCurrencies.php
+++ web/project01/cryptoCurrencies.php
@@ -47,11 +47,11 @@ $(document).ready(function() {
 	
 		<form action="cryptoCurrencies.php" method="post">
 			<input type="hidden" name="id"/>
-			<div class="form-group">
+			<div class="w-25" class="form-group">
 				<label for="code">Coin Code:</label>
 				<input type="text" class="form-control" name="code">
 			</div>
-			<div class="form-group">
+			<div class="w-25 class="form-group">
 				<label for="name">Coin Name:</label>
 				<input type="text" class="form-control" name="name">
 			</div>

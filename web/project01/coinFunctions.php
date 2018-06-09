1b8fc5f36465faa39d5836153674c58156cf017e
Leonilson <leonilson.lopes1@gmail.com>
2018-06-09 01:07:13 +0100

    week06 project01

--- web/project01/coinFunctions.php
+++ web/project01/coinFunctions.php
@@ -1,10 +1,11 @@
 <?php
 function getCoinInfo($coinCode){
+	echo "<br/>Received coinCode: " . $coinCode;
 	$coinbasePublicAPI = 'https://api.coinmarketcap.com/v1/ticker/';
 	$coinData = file_get_contents($coinbasePublicAPI);
 	$coinData = json_decode($coinData, true);
 	$numCoinbaseCoins = sizeof ($coinData);
-	
+	echo "<br/>dump: " . var_dump(coinData);
 	for ( $i=0; $i<$numCoinbaseCoins; $i++) {   
 		if(strtoupper($coinCode) == strtoupper($coinData[$i]['symbol'])){
 			$coinData[$i]['price_usd'] = '$' . $coinData[$i]['price_usd'];
